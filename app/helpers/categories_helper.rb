fa1e775d0863429384323cc5946a39855cefbff7
cblair <cblair@vandals.uidaho.edu>
2012-09-23 14:51:39 -0700

    Added recursive select_options helpers.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -19,4 +19,28 @@ module CategoriesHelper
       child_c.destroy
     end
   end
+
+  #Get all category options, with indentation  
+  def get_all_category_select_options()
+    o = []
+    Category.where(:category_id => nil).each do |c|
+      get_category_select_options(c).each do |c_option|
+        o << c_option
+      end
+    end
+    return o
+  end
+  
+  #Makes form select_options, indenting the children
+  def get_category_select_options(c, level=0)
+    retval = []
+    retval << [('-' * level) + c.name, c.id]
+    c.categories.each do |child_c|
+      get_category_select_options(child_c, level + 1).each do |child_c|
+        retval << child_c
+      end
+    end
+    
+    return retval
+  end
 end
